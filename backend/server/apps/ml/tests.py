1eadccc5148e5e3554ced53e433fe03d47d20e0d
Huang Yu <1262993208@qq.com>
2023-09-20 14:15:40 +0800

    add ML code and algorithms

--- backend/server/apps/ml/tests.py
+++ backend/server/apps/ml/tests.py
@@ -1,6 +1,7 @@
 # add at the beginning of the file:
 import inspect
 from apps.ml.registry import MLRegistry
+from apps.ml.income_classifier.extra_trees import ExtraTreesClassifier
 
 
 '''# add below method to MLTests class:
@@ -28,6 +29,29 @@ from django.test import TestCase
 from apps.ml.income_classifier.random_forest import RandomForestClassifier
 
 class MLTests(TestCase):
+    def test_et_algorithm(self):
+        input_data = {
+            "age": 37,
+            "workclass": "Private",
+            "fnlwgt": 34146,
+            "education": "HS-grad",
+            "education-num": 9,
+            "marital-status": "Married-civ-spouse",
+            "occupation": "Craft-repair",
+            "relationship": "Husband",
+            "race": "White",
+            "sex": "Male",
+            "capital-gain": 0,
+            "capital-loss": 0,
+            "hours-per-week": 68,
+            "native-country": "United-States"
+        }
+        my_alg = ExtraTreesClassifier()
+        response = my_alg.compute_prediction(input_data)
+        self.assertEqual('OK', response['status'])
+        self.assertTrue('label' in response)
+        self.assertEqual('<=50K', response['label'])
+
     def test_registry(self):
         registry = MLRegistry()
         self.assertEqual(len(registry.endpoints), 0)
